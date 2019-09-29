17703066051115616a28cad2d57db021109aa166
James <jingzl@hotmail.com>
2019-09-29 20:54:34 +0800

    init stockfilter server file

---
+++ stockfilter.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 29 17:50:44 2019
+
+@author: jingzl
+
+stockfilter.py
+
+"""
+import os
+import sys
+
+# import libraries in lib directory
+base_path = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(base_path, 'lib'))
+
+
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello'
+
+
