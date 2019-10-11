ebef9d13b6baa3c136e66b0b69ef8c19fe61c532
James <jingzl@hotmail.com>
2019-10-11 20:00:31 +0800

    add filtermgr

---
+++ stkfilter/filtermgr.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 11 18:52:17 2019
+
+@author: jingzl
+
+filtermgr.py
+"""
+
+class FilterMgr( object ):
+    
+    def __init__( self ):
+        pass
+    
+    def version( self ):
+        return "v0.1"
+    
+    def stockfilter( self ):
+        return "f098213123ssfsdf8890"
+
+    def querypos( self ):
+        return "50"
+
+
+
+
+
+
